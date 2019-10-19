4b7f661be531469de20a088ce14a3107b5e464ce
Antti Oksman <antti@juicy.games>
2019-10-19 15:05:46 +0300

    working ngram analyzer
    fixed all the bugs in analyzer and now returns different valeus for crypted vs decrypted

--- ngramscore.py
+++ ngramscore.py
@@ -11,8 +11,8 @@ class NgramScore(object):
             for line in ngramFileHandler:
                 key, count = line.split(sep)
                 self.ngrams[key] = int(count)
-        # eli täs kohtaa pitäs laskee per avain pituus
-        self.L = len(self.ngrams.keys())
+        # assume all keys are same length in this case
+        self.keyLength = len(list(self.ngrams.keys())[0])
         self.N = sum(self.ngrams.values())
         # calculate log probabilities
         for key in self.ngrams.keys():
@@ -22,22 +22,18 @@ class NgramScore(object):
     def score(self, text):
         """ compute the score of text """
         score = 0
-        ngram_items = self.ngrams.__getitem__
-        print("typeof items: ", type(ngram_items))
-        self.ngrams
-        # täs haetaan et jos tää määrä kirjaimii täst lauseest löyty ni lisätä score
-        for i in range(len(text)):
-            if i in self.ngrams:
-                print("HIT")
-                score += self.ngrams.__getitem__(text[i:i + self.L])
+        # check letter by letter until the last valid combo
+        for i in range(len(text) - self.keyLength + 1):
+            searchValue = text[i:i + self.keyLength].upper()
+            if searchValue in self.ngrams:
+                score += self.ngrams[searchValue]
             else:
                 score += self.floor
         return score
 
 
-ns_fitness = NgramScore('finnish_monograms.txt')
-print("typeOf:", type(ns_fitness))
-# ns_fitness = NgramScore('finnish_quadgrams.txt')
+# ns_fitness = NgramScore('finnish_monograms.txt')
+ns_fitness = NgramScore('finnish_quadgrams.txt')
 # ns_fitness = NgramScore('english_quadgrams.txt')
 uncrypted = 'köunidm oöqjdsdhkkö nm jåxsx oxäqågsdkdlårrå dmshrså kööidllöksö öktddksö iö sömrrhinhcdm intjnrrö nm nkkts öhdloöö dmdllåm mtnqdloöö onkudö.'
 test_message = 'lavojen parketeilla on käyty pyörähtelemässä entistä laajemmalta alueelta ja tanssijoiden joukossa on ollut aiempaa enemmän nuorempaa polvea.'
